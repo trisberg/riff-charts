23da33eed18b15db544841cecbd464b77bce785f
Scott Andrews <sandrews@pivotal.io>
2019-10-23 16:53:23 -0400

    Consume latest FATS with updated no-resource-requests macro (#65)

--- ci/install-riff.sh
+++ ci/install-riff.sh
@@ -28,13 +28,7 @@ source $FATS_DIR/macros/helm-init.sh
 echo "Install Cert Manager"
 helm install ${certmanager_chart} --name cert-manager --wait
 
-# TODO go back to the macro once FATS is updated for the cert-manager chart
-# source $FATS_DIR/macros/no-resource-requests.sh
-if [ $(kubectl get nodes -oname | wc -l) = "1" ]; then
-  echo "Elimiate pod resource requests"
-  fats_retry kubectl apply -f https://storage.googleapis.com/projectriff/no-resource-requests-webhook/no-resource-requests-webhook.yaml
-  wait_pod_selector_ready app=webhook no-resource-requests
-fi
+source $FATS_DIR/macros/no-resource-requests.sh
 
 if [ $RUNTIME = "knative" ]; then
   echo "Install Istio"
