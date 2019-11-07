7c3cf6590f37bcdc84bfa87486ad34a858f9f920
Scott Andrews <sandrews@pivotal.io>
2019-11-07 15:01:04 -0500

    Consume helperless FATS (#82)

--- .github/workflows/run-tests.sh
+++ .github/workflows/run-tests.sh
@@ -5,20 +5,26 @@ set -o nounset
 set -o pipefail
 
 source $FATS_DIR/.configure.sh
-source $FATS_DIR/functions/helpers.sh
 
 # setup namespace
 kubectl create namespace $NAMESPACE
 fats_create_push_credentials $NAMESPACE
 
 for test in java java-boot node npm command; do
-  path="$FATS_DIR/functions/uppercase/${test}"
-  function_name=fats-cluster-uppercase-${test}
-  image=$(fats_image_repo ${function_name})
-  create_args="--git-repo https://github.com/${FATS_REPO}.git --git-revision ${FATS_REFSPEC} --sub-path functions/uppercase/${test}"
-  input_data=charts
-  expected_data=CHARTS
-  runtime=$RUNTIME
+  name=fats-cluster-uppercase-${test}
+  image=$(fats_image_repo ${name})
 
-  run_function $path $function_name $image "$create_args" $input_data $expected_data $runtime
+  echo "##[group]Run function $name"
+
+  riff function create $name --image $image --namespace $NAMESPACE --tail \
+    --git-repo https://github.com/$FATS_REPO --git-revision $FATS_REFSPEC --sub-path functions/uppercase/${test} &
+
+  riff $RUNTIME deployer create $name --function-ref $name --namespace $NAMESPACE --tail
+  source ${FATS_DIR}/macros/invoke_${RUNTIME}_deployer.sh $name "-H Content-Type:text/plain -H Accept:text/plain -d cli" CLI
+  riff $RUNTIME deployer delete $name --namespace $NAMESPACE
+
+  riff function delete $name --namespace $NAMESPACE
+  fats_delete_image $image
+
+  echo "##[endgroup]"
 done
