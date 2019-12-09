3859943858215f506fa731f0c9ff6916fdc11a00
Scott Andrews <sandrews@pivotal.io>
2019-12-09 17:47:25 -0500

    Publish latest System (#114)
    This introduces two breaking changes:
    - PodTemplateSpec has replaced PodSpec at `.spec.template`. The PodSpec
      is now available at `.spec.template.spec`
    - `.status.*Name` fields are now references named `.status.*Ref`. The
      name is available at `.status.*Ref.name`. Where the name was
      previously an empty string, the reference is nil.

--- .github/workflows/run-tests.sh
+++ .github/workflows/run-tests.sh
@@ -9,10 +9,13 @@ source $FATS_DIR/.configure.sh
 # setup namespace
 kubectl create namespace $NAMESPACE
 fats_create_push_credentials $NAMESPACE
+source ${FATS_DIR}/macros/create-riff-dev-pod.sh
 
 for test in java java-boot node npm command; do
   name=fats-cluster-uppercase-${test}
   image=$(fats_image_repo ${name})
+  curl_opts="-H Content-Type:text/plain -H Accept:text/plain -d charts"
+  expected_data="CHARTS"
 
   echo "##[group]Run function $name"
 
@@ -24,7 +27,20 @@ for test in java java-boot node npm command; do
     --ingress-policy External \
     --namespace $NAMESPACE \
     --tail
-  source ${FATS_DIR}/macros/invoke_${RUNTIME}_deployer.sh $name "-H Content-Type:text/plain -H Accept:text/plain -d charts" CHARTS
+  if [ $RUNTIME = "core" ]; then
+    # TODO also test external ingress for core runtime
+    source ${FATS_DIR}/macros/invoke_incluster.sh \
+      "$(kubectl get deployers.${RUNTIME}.projectriff.io ${name} --namespace ${NAMESPACE} -ojsonpath='{.status.address.url}')" \
+      "${curl_opts}" \
+      "${expected_data}"
+  fi
+  if [ $RUNTIME = "knative" ]; then
+    # TODO also test clusterlocal ingress for knative runtime
+    source ${FATS_DIR}/macros/invoke_${RUNTIME}_deployer.sh \
+      $name \
+      "${curl_opts}" \
+      "${expected_data}"
+  fi
   riff $RUNTIME deployer delete $name --namespace $NAMESPACE
 
   riff function delete $name --namespace $NAMESPACE
