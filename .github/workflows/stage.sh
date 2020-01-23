d65e50657a60444a965f2e610bbd6e122ce17ba7
Scott Andrews <sandrews@pivotal.io>
2020-01-23 17:00:02 -0500

    restore helm init for staging

--- .github/workflows/stage.sh
+++ .github/workflows/stage.sh
@@ -9,6 +9,7 @@ readonly git_sha=$(git rev-parse HEAD)
 readonly git_timestamp=$(TZ=UTC git show --quiet --date='format-local:%Y%m%d%H%M%S' --format="%cd")
 readonly slug=${version}-${git_timestamp}-${git_sha:0:16}
 
+helm init --client-only
 make clean package
 
 # upload releases
